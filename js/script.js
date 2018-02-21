5a13f695a0df706df91f55536e40a0720bb63dee
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-02-21 12:42:18 +0200

    optimize js

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ const brakeChain = () => {
 brakeChain();
 setInterval(brakeChain, 2000);
 
+const changeIcon = (icon, type, time) => setTimeout(() => icon.innerHTML = type, time);
+
 const chargeBattery = () => {
   const battery = document.getElementById('battery');
   battery.innerHTML = "&#xf244;"
 
-  const changeIcon = (icon, time) => setTimeout(() => battery.innerHTML = icon, time);
-
-  changeIcon("&#xf243;",1000);
-  changeIcon("&#xf242;",2000);
-  changeIcon("&#xf241;",3000);
-  changeIcon("&#xf240;",4000);
+  changeIcon(battery, "&#xf243;", 1000);
+  changeIcon(battery, "&#xf242;", 2000);
+  changeIcon(battery, "&#xf241;", 3000);
+  changeIcon(battery, "&#xf240;", 4000);
 }
 
 chargeBattery();
@@ -27,10 +27,8 @@ const hourglassTip = () => {
   const hourglass = document.getElementById('hourglass');
   hourglass.innerHTML = "&#xf251;"
 
-  const changeIcon = (icon, time) => setTimeout(() => hourglass.innerHTML = icon, time);
-
-  changeIcon("&#xf252;",1000);
-  changeIcon("&#xf253;",2000);
+  changeIcon(hourglass, "&#xf252;", 1000);
+  changeIcon(hourglass, "&#xf253;", 2000);
 }
 
 hourglassTip();
